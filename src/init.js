dbf476d49f992d5e34fc20bb7eab5af99d99bb25
Jason-Mati <matigevic@gmail.com>
2022-05-29 19:01:01 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -8,7 +8,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
